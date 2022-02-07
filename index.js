9524baec92c309754903649e30e37cd58196a663
Little Jacob <joelnoumbi28@gmail.com>
2022-02-07 16:17:05 +0100

    server deployment

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
+import dotenv from 'dotenv';
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+app.use('/', (req, res) => {
+    res.send("Yo men ! go to /post to see the data 😁")
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECT_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((e) => console.log(e.message))
