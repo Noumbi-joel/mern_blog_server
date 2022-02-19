bfa92bbbb1c942023e6482c7482eb7985831a853
Little Jacob <joelnoumbi28@gmail.com>
2022-02-19 05:53:36 +0100

    server v0.1.0

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-
 app.use("/posts", postRoutes);
 app.use("/profile", profileRoutes);
 app.use("/users", userRoutes);
@@ -32,7 +31,7 @@ app.use("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.CONNECT_URL, {
+  .connect(`${process.env.CONNECT_URL}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
