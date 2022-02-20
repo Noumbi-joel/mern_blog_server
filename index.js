88729ea5aa7b1647ee965cfa9cc8703a84726319
Little Jacob <joelnoumbi28@gmail.com>
2022-02-20 02:53:35 +0100

    dot env modif

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.CONNECT_URL, {
+  .connect(String(process.env.CONNECT_URL), {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
